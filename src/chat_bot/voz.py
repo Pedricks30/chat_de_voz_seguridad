95313fdd189062bb38b539f53e21082292648045
Pedrick <eplopez-es@udabol.edu.bo>
2025-06-12 00:03:17 -0400

    Agregue las normas bolivianas

--- src/chat_bot/voz.py
+++ src/chat_bot/voz.py
@@ -5,8 +5,8 @@ import os
 import re  # nuevo: para limpiar texto
 
 def limpiar_texto_para_audio(texto):
-    # Quita los asteriscos (*) y reemplaza dobles con comillas si lo deseas
-    texto = re.sub(r'\*+','\#+', '', texto)
+    # Elimina todos los asteriscos (*) y almohadillas (#)
+    texto = re.sub(r'[\*#]+', '', texto)
     return texto
 
 def generar_audio(texto, filename="respuesta.mp3"):
